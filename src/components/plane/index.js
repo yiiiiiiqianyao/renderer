2f02d38ef2d106785face76a6320b2089aa49644
shihui <yiqianyao.yqy@alibaba-inc.com>
2021-09-27 00:41:38 +0800

    feat: 增加简单的材质系统，同时增加简单的纹理系统

--- src/components/plane/index.js
+++ src/components/plane/index.js
@@ -7,8 +7,11 @@ export default class Plane extends Group{
     constructor(props) {
         super(props)
         this.type = 'PlaneMesh'
+
+        this.firstLoad = true
         
-        this.camera = props.camera
+        this.camera = props?.camera
+        this.material = props?.material
        
         this.width = 1
         this.height = 0.5
@@ -38,13 +41,13 @@ export default class Plane extends Group{
         ])
         var FSIZE = rectVertices.BYTES_PER_ELEMENT
 
-        
         this.setMatrixs()
 
         let { attr: a_PositionLocation } = glUtils.bindAttriBuffer(this.gl, 'a_Position', rectVertices, 2, this.program)
         let { attr: a_TextCoordLocation } = glUtils.bindAttriBuffer(this.gl, 'a_TextCoord', rectUvs, 2, this.program)
-    }
 
+        this.draw()
+    }
 
     /**
      * 设置当前网格的矩阵、同时将矩阵传递给着色器
@@ -72,6 +75,13 @@ export default class Plane extends Group{
         this.shaderUnifroms.map(({location, currentDataLocation}) => {
             glUtils.setUnifrom4fv(this.gl, location, currentDataLocation)
         })
+
+        // TODO: 每次渲染的时候重新为纹理分配纹理空间
+        if(this.texture) {
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture)
+            var u_Sampler = this.gl.getUniformLocation(this.program, 'u_Sampler');
+            this.gl.uniform1i(u_Sampler, 0);
+        }
     }
 
     /**
@@ -91,6 +101,7 @@ export default class Plane extends Group{
      * 绘制当前的网格对象
      */
     draw() {
+     
         this.gl.useProgram(this.program)
 
         this.updateShaderUnifroms()
@@ -110,9 +121,9 @@ export default class Plane extends Group{
 
             attribute vec4 a_Position;
             attribute vec2 a_TextCoord;
-            varying vec2 v_TexCoord;
+            varying vec2 v_uv;
             void main(){
-                v_TexCoord = a_TextCoord;
+                v_uv = a_TextCoord;
 
                 gl_Position = u_projMatrix * u_viewMatrix *  u_modelMatrix * a_Position;
            
@@ -125,12 +136,41 @@ export default class Plane extends Group{
      * @returns 
      */
     getRectFSHADER(){
-        return `
-            precision mediump float;
-            varying vec2 v_TexCoord;
+        let firstLine = 'precision mediump float;\n'
+        let gl_FragColorLine = 'gl_FragColor = vec4(v_uv, 1.0, 1.0);\n'
+        let unifromLines = []
+        if(this?.material?.map) {
+
+            this.material?.on('loadImage', ({texture, img}) => {
+                this.gl.useProgram(this.program)
+
+                // TODO: cache texture
+                this.texture = texture
+           
+                this.gl.activeTexture(this.gl.TEXTURE0); // 激活0号纹理单元
+                this.material.texture && this.gl.bindTexture(this.gl.TEXTURE_2D, texture); // 绑定纹理单元
+    
+                var u_Sampler = this.gl.getUniformLocation(this.program, 'u_Sampler');
+                this.gl.uniform1i(u_Sampler, 0);
+    
+                this.draw()
+            })
+            gl_FragColorLine = 'gl_FragColor = texture2D(u_Sampler, v_uv);\n'
+        }
+        const shader = 
+            firstLine + 
+            unifromLines.join('') +
+            `
+            uniform sampler2D u_Sampler;
+            varying vec2 v_uv;
             void main(){
-                gl_FragColor =  vec4(v_TexCoord, 0.0, 1.0);
+                `+
+                gl_FragColorLine
+                +`
             }
         `
+        return shader;
+
     }
 }
+
